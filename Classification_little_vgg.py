282926ca1716caa371d7ec23f42f55d881cd5004
jatinkumar11954 <jatinkumar11954@gmail.com>
2020-06-16 13:51:05 +0530

    trying

--- Classification_little_vgg.py
+++ Classification_little_vgg.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense,Dropout,Activation,Flatten,BatchNormalization
 from keras.layers import Conv2D,MaxPooling2D
 import os
-
+jatin=5
 num_classes = 5
 img_rows,img_cols = 48,48
 batch_size = 32
